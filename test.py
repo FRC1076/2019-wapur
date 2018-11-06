bd7b19758b88a87fc7320bb6635575fc02acce73
C Jones <code@calebjones.net>
2018-11-05 22:48:19 -0500

    Make test.py only work in Python 3.7+

--- test.py
+++ test.py
@@ -1,1 +1,20 @@
+"""
+Hi!
+I intentionally wrote this script sort of weird so that it will
+only work in Python 3.7+, to make sure that you're using
+the right Python. Don't worry if you don't understand it!
+If you're curious, I'm happy to explain anything in it.
+
+-- C
+"""
+from __future__ import annotations
+
 print("Hello, ", end='World!\n')
+
+
+def foo(x: X) -> None:
+    pass
+
+
+class X:
+    pass
