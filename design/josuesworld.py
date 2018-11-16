3d7f2ffd10b210a2b2a13765a352d986037b87e0
Joshua Withers <joshue@Matthews-MacBook.local>
2018-11-15 19:58:15 -0500

    pythonknowledge

---
+++ design/josuesworld.py
@@ -0,0 +1,17 @@
+x = 3
+if x == 5:
+    print("I am five")
+else:
+    print("I am not five")
+
+teams = ["1076,", "803", "1234"]
+teams = ["1076," "803", "1234"]
+query = 1076
+for team in teams:
+    if query == team:
+        print("Query has been found!")
+
+count = 0
+while count < 5:
+    print(count)
+    count += 1
