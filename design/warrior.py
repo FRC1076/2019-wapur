dea5a8e2f839d22a82106b87039446f2b66dd08d
Karinne Tennenbaum <karinnetennenbaum@Karinnes-MacBook-Air.local>
2018-11-15 19:54:58 -0500

    Do the Crashcourse Introduction

---
+++ design/warrior.py
@@ -0,0 +1,79 @@
+# typo = 7
+# floater = 7.8
+# print(typo)
+# print(floater)
+
+# one = 1
+# two = 2
+# three = one + two
+# print(three)
+
+# hello = "hello"
+# world = "world"
+# helloworld = hello + " " + world
+# print(helloworld)
+
+# a, b = 3, 4
+# print(a, b)
+
+# x = [1, 3, 4, 7, 4, 9]
+# print(x[0], x[4])
+
+# x = 15 ** 2
+# print(x)
+
+# lotsofhellos = "hello" * 30
+# print(lotsofhellos)
+
+# even_numbers = [2, 4, 6, 8]
+# odd_numbers = [1, 3, 5, 7]
+# all_numbers = sorted(odd_numbers + even_numbers)
+# print(all_numbers)
+# print([1, 2, 3] * 3)
+
+# x_list = ["John", "Kara", "Rachel", "Ally"]
+# y_list = ["Hannah", "Fred", "Don", "Charlie", "Roberta"]
+# names_list = sorted(x_list + y_list)
+# print(names_list)
+
+# name = "John"
+# age = 23
+# if name == "John" and age == 23:
+#     print("Your name is John, and you are 23 years old.")
+# if name == "John" or name == "Rick"
+#     print("Your name is either John or Rick.")
+
+# x = 1600
+# if x == 1600:
+#     print("My SAT score was perfect!")
+# elif x == 36:
+#     print("My ACT score was perfect!")
+# else:
+#     print("Happy New Year!")
+
+# family = ["Charlie", "Mia", "Chris", "Jan"]
+# if "Charlie" in family:
+#     print("Yes")
+# elif "Mia" in family:
+#     print("2nd yes")
+# else:
+#     print("No")
+
+# for x in range(5):
+#     print(x)
+
+# for x in range(3, 6):
+#     print(x)
+
+# for x in range(3, 13, 2):
+#   print(x)
+
+# people = ["Matthew", "Mark", "Luke", "John"]
+# for person in people:
+#     query = "Matthew"
+#     if query == person:
+#         print("Query has been found!")
+
+teams = [1076, 803, 1234]
+for team in teams:
+    print(team, "is going to worlds")
