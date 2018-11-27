e872489792e9573b7205eccde55be21705af6e3c
Danniell Hu <dannihu01@gmail.com>
2018-11-27 17:06:26 -0500

    Update

--- code_practice/functions.py
+++ code_practice/functions.py
@@ -1,40 +1,44 @@
 import functions_variables
+import random
+
 # Given a direction the robot is facing, find the direction
 # that the robot would then be facing if the robot turned right.
+
+init_direction = random.randint(0, 3)
+fin_direction_right = None
+fin_direction_left = None
+
 NORTH = 0
 EAST = 1
 SOUTH = 2
 WEST = 3
 
-# Do tests and set a result using if/else if 
+#-------------------------------------------------------------------#
+# Do tests and set a result using if/else if... what direction is it facing if it turns right?
 
-if (direction = 0)
+#print your final direction
+
+#-------------------------------------------------------------------#
+# Now try a solution that uses modulus
+
+#print your final direction
+
+#-------------------------------------------------------------------#
+# How do we compute what left facing is?
 
 
+#-------------------------------------------------------------------#
+# Put your algorithms inside functions
+def direction_to_right(direction):
+    ...
 
 
+def direction_to_left(direction):
+    ...
 
 
+# Use our mapping function!
+to_right = [EAST, SOUTH, WEST, NORTH]
+to_left = [WEST, NORTH, EAST, SOUTH]
 
-
-
-# Now try a solution that's a single expression and uses modulus
-right = (facing + 1) % 4
-
-# Then see if they can figure out how to compute what left
-# facing is from this.
-left = ???
-
-# Then discuss how you can use an array to do a mapping
-# function. (provided elements can be used as an index)
-# See if they can figure it out.
-to_right = [ EAST, SOUTH, WEST, NORTH ]
-to_left = [ WEST, NORTH, EAST, SOUTH ]
-
-# You could also show them how to use a dictionary to store
-# the mapping.  (if dictionaries are on the agenda)
-
-# Then show them how to put this inside a couple of functions
-# so that it is easily reused.
-def direction_to_right(dir):
-	...
+# Dictionaries to store arrays?
