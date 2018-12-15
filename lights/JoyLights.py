e453282768fd11c4118f144c623f7a2bde1ced9d
= <=>
2018-12-15 15:41:41 -0500

    test

--- lights/JoyLights.py
+++ lights/JoyLights.py
@@ -3,6 +3,22 @@ import udp_channels as udp
 from Line import get_line
 
 
+def cart_to_neo(location):
+    x, y = location
+    #check if even
+
+    if (x % 2 == 0):
+        #x is even
+        val = (x * 8) + (7 - y)
+
+    else:
+        #x is odd
+        val = (x * 8) + y
+
+    #print(val)
+    return val
+
+
 class JoyLights:
     def __init__(self, ard_addr, joystick):
         # create a socket to send to the arduion
@@ -24,38 +40,17 @@ class JoyLights:
 
         if (self.counter == 10):
             self.counter = 0
-            data = {
-                'sender': 'joystick',
-                'message': 'joy',
-                'x': x,
-                'y': y,
-            }
+
+            l = get_line(start=(0, 0), end=(round(4 * 8), round(4 * 8)))
+
+            for a in l:
+                w = cart_to_neo(location=a)
+
+            data = {'sender': 'joystick', 'message': 'joy', 'values': w}
 
             message = json.dumps(data)
 
             print(message)  #for debuging
-            self.sender.send_to(message)
+            self.sender.send_to(message)s
         else:
             self.counter += 1
-
-
-def cart_to_neo(location):
-    x, y = location
-    #check if even
-
-    if (x % 2 == 0):
-        #x is even
-        val = (x * 8) + (7 - y)
-
-    else:
-        #x is odd
-        val = (x * 8) + y
-
-    print(val)
-    return val
-
-
-l = get_line(start=(0, 0), end=(round(4 * 8), round(4 * 8)))
-
-for a in l:
-    cart_to_neo(location=a)
